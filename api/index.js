e55994528059bd0d78128f5176a9fab9cfbcc8a1
radiantly <buildingsomethingfun@gmail.com>
2021-08-07 21:04:14 +0530

    Parse form fields and add to body

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const fastify = require("fastify")({ logger: true });
 
-fastify.register(require("fastify-multipart"));
+fastify.register(require("fastify-multipart"), {
+  attachFieldsToBody: true,
+});
 fastify.register(require("./routes"), { prefix: "/api" });
 
 fastify.listen(5000);
--- api/routes.js
+++ api/routes.js
@@ -1,23 +1,24 @@
 module.exports = function (fastify, opts, done) {
   fastify.post("/email", async (req, res) => {
-    const body = req.body;
-    console.log("to: ", body.to);
-    console.log("cc: ", body.cc);
-    console.log("from: ", body.from);
-    console.log("subject: ", body.subject);
+    console.log(req.body);
+    // const body = req.body;
+    // console.log("to: ", body.to);
+    // console.log("cc: ", body.cc);
+    // console.log("from: ", body.from);
+    // console.log("subject: ", body.subject);
 
-    console.log("html: ", body.html);
-    console.log("text: ", body.text);
+    // console.log("html: ", body.html);
+    // console.log("text: ", body.text);
     // console.log("attachments: ", body.attachments);
     // console.log("attachment-info: ", body['attachment-info']);
     // console.log("content-ids: ", body['content-ids']);
 
-    if (req.files.length > 0) {
-      // Log file data
-      console.log(req.files);
-    } else {
-      console.log("No files...");
-    }
+    // if (req.files.length > 0) {
+    //   // Log file data
+    //   console.log(req.files);
+    // } else {
+    //   console.log("No files...");
+    // }
 
     return res.status(200).send();
   });

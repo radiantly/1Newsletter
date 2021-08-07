90dec6224152dc7b909182e516c1a1b3bae217c2
radiantly <buildingsomethingfun@gmail.com>
2021-08-07 20:42:39 +0530

    Add multipart form support

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const fastify = require("fastify")({ logger: true });
 
+fastify.register(require("fastify-multipart"));
 fastify.register(require("./routes"), { prefix: "/api" });
 
 fastify.listen(5000);
